b1f62dfdd21af76bea86c4303bbeb1f08a497327
holma198 <holma198@morris.umn.edu>
2017-10-18 16:37:01 -0500

    Make progress on file_disemvowel so that it now properly reads files
    In fact, it does the opposite of what we want. Instead of writing only consonants to the output string, it writes only vowels!

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -26,26 +26,59 @@ int copy_non_vowels(int num_chars, char* in_buf, char* out_buf){
 	return copyCount;	
 }
 
-void disemvowel(FILE* inputFile, FILE* outputFile) {
+void disemvowelOneArg(FILE* inputFile) {
 	char* in_buf = calloc(BUF_SIZE, sizeof(char));
 	char* out_buf = calloc(BUF_SIZE, sizeof(char));
-	fopen(inputFile, "w+");
-	fopen(outputFile, "w+");
+	fopen(inputFile, "r");
 	while(fgets(in_buf,sizeof(in_buf),inputFile)){
-		printf("%s\n",in_buf);
+		
 	}
-	fread(in_buf, 1024, 1, inputFile);
-	
 	
 }
 
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+	char* in_buf = calloc(BUF_SIZE, sizeof(char));
+	char* out_buf = calloc(BUF_SIZE, sizeof(char));
+	//fopen(inputFile, "r");
+	//fopen(outputFile, "w+");
+	while(fgets(in_buf,sizeof(in_buf),inputFile) != NULL){
+		printf("%s\n",in_buf);
+	
+	
+		
+	
+	int count = 0;
+	while(in_buf[count] != '\0'){
+		count++;
+	}
+	copy_non_vowels(count, in_buf, out_buf);
+	//fread(in_buf, 1024, 1, inputFile);
+	if(outputFile != NULL){
+		printf("%s\n","putting stuff");
+		fputs(out_buf, outputFile);
+		
+	}
+}
+fclose(outputFile);
+}
 
+
+
+// fix definition of input/output files, need to be full file names not single characters.
 int main(int argc, char *argv[]) {
+ printf("%s\n",argv[1]);
  FILE *inputFile;
  FILE *outputFile;
-
-//disemvowel(inputFile, outputFile);
- 
+if(argc > 2){
+	inputFile = fopen(argv[1], "r");
+	outputFile = fopen(argv[2], "w+");
+	//argv[1] = inputFile;
+	//argv[2] = outputFile;
+	disemvowel(inputFile, outputFile);
+} else if(argc == 2){
+	argv[1] = inputFile;
+	disemvowel(inputFile, inputFile);
+}
  return 0;
 }
 
