b3f582d30d807211b47f9eb0ac8fafaa4ed0cd20
holma198 <holma198@morris.umn.edu>
2017-10-19 11:16:12 -0500

    Complete code for summarize_tree_ftw, which recursively counts the numbers of files and folders in a directory.

---
+++ summarize_tree/summarize_tree_ftw.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <ftw.h>
+int dirCount = 0;
+int fileCount = 0;
+
+//function to be recursed in the ftw command, and increments the file and directory variables, and returns 0 to ensure error is not read into ftw.
+static int callback(const char *fpath, const struct stat *sb, int typeflag) {
+	
+        	
+	if(typeflag==FTW_D){
+		dirCount++;
+		return 0; 		
+	}
+	if(typeflag==FTW_F){
+		fileCount++;
+		return 0;
+	}
+	return 0;
+}
+
+#define MAX_FTW_DEPTH 16
+
+//method that runs ftw to recursively call callback for each folder in the directory given.
+int main(int argc, char** argv) {
+
+	ftw(argv[1], callback, MAX_FTW_DEPTH);
+	printf("Processed all the files from %s. \n",argv[1]);
+	printf("There were %d directories. \n",dirCount);
+	printf("There were %d regular files. \n", fileCount);
+
+	return 0;
+}
